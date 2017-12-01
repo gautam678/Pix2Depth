9f73e21257674571ef42e4ae62d53a04472fde12
Marzieh <ml2au@virginia.edu>
2017-11-30 23:33:15 -0500

    before pull after GAN

--- src/model/GeneratorForErrorMap.py
+++ src/model/GeneratorForErrorMap.py
@@ -18,8 +18,11 @@ def div0( a, b ):
         c[ ~ np.isfinite( c )] = 1  # -inf inf NaN
     return c
 
-def GeneratorErrorMap(img_dim_tuple,batch_size=10, modelEpoch=130, threshold=0.1):
-   gen , length = fDataSet_generator(img_dim, batch_size)
+def GeneratorErrorMap(img_dim_tuple,batch_size=10, modelEpoch=130, threshold=0.1,f=None,indexes=1448):
+   if(f==None):
+   	path = "../../data/nyu_depth_v2_labeled.mat"
+   	f = h5py.File(path)
+   gen , length = fDataSet_generator(img_dim, batch_size,f=f)
    model = generator_unet_upsampling(img_dim, bn_mode, batch_size)
    model.load_weights('../../models/CNN/pix2depthgen_weights_epoch%d.h5' % modelEpoch) #gen_weights_epoch45.h5
    def GenFunction():
@@ -28,7 +31,8 @@ def GeneratorErrorMap(img_dim_tuple,batch_size=10, modelEpoch=130, threshold=0.1
            dmap = model.predict(img)
 #           errorMap=(np.divide(np.fabs(dmap-dep), dep)<threshold)
 #           errorMap=(np.divide(np.fabs(dmap-dep), dep)<threshold).astype(int)
-           errorMap=(div0(np.fabs(dmap-dep), dep)<threshold).astype(int)
+#           errorMap=(div0(np.fabs(dmap-dep), dep)<threshold).astype(int)
+           errorMap=(np.fabs(dmap-dep)<threshold).astype(int)
            yield (img,errorMap)
    return(GenFunction(),1448) #todo: accurate Value
 
